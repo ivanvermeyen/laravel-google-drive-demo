c872c11c3aef822f20a9e952ae89cc8dea92746a
Ivan Vermeyen <ivan@vermeyen.org>
2017-07-20 20:54:45 +0200

    Install Google Drive storage driver

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,6 +63,14 @@ return [
             'bucket' => env('AWS_BUCKET'),
         ],
 
+        'google' => [
+            'driver' => 'google',
+            'clientId' => env('GOOGLE_DRIVE_CLIENT_ID'),
+            'clientSecret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+            'refreshToken' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+            'folderId' => env('GOOGLE_DRIVE_FOLDER_ID'),
+        ],
+
     ],
 
 ];
